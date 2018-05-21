78ce16d9765c07638f2faf27d507708476cf6473
thikon-nb <thikon@gmail.com>
2018-05-21 22:20:04 +0700

    sample

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sample_ci_vsts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
